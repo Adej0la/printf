c43a55f4a0256162666b617b7ebce9faaa60f891
Adej0la <recursr.dev@gmail.com>
2022-11-23 23:37:42 +0000

    Create array of format type structs

--- printf.c
+++ printf.c
@@ -4,10 +4,25 @@ int _printf(const char *format, ...)
 {
 	va_list ap;
 	int c_count;
+	fmt_t f_arr[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"%", print_percent},
+		{"d", print_decimal},
+		{"i", print_int},
+		{"r", print_rev},
+		{"b", print_bin},
+		{"R", rot13},
+		{"u", print_uint},
+		{"o", print_octal},
+		{"x", print_lhex},
+		{"X", print_uhex},
+		{NULL, NULL}
+	}
 
 	va_start(ap, format);
 
-	c_count = _parser(format, f_list, ap);
+	c_count = _parser(format, f_arr, ap);
 
 	va_end(ap);
 
